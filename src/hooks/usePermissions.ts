a42855116d10128f75d7314d80e04b9babdd6f42
John Hillert <john.hillert@gmail.com>
2023-06-14 13:33:48 -0500

    ios FCM push notifications setup

--- src/hooks/usePermissions.ts
+++ src/hooks/usePermissions.ts
@@ -1,9 +1,23 @@
 import { useEffect } from 'react';
 import { PermissionsAndroid } from 'react-native';
-import { isAndroid } from '../constants';
+import { isAndroid, isIos } from '../constants';
+import messaging from '@react-native-firebase/messaging';
 
 const useNotificationsPermission = () => {
   useEffect(() => {
+    const requestUserPermission = async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (enabled) {
+        console.log('Authorization status:', authStatus);
+      }
+    };
+
+    isIos && requestUserPermission();
+
     isAndroid &&
       PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
