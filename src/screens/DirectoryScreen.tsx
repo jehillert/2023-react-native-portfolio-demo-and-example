3371273e124dcabdd51c78ca1875c35852762965
John Hillert <john.hillert@gmail.com>
2023-06-16 18:51:56 -0500

    <NoteScreen /> ==> RichTextEditor

--- src/screens/DirectoryScreen.tsx
+++ src/screens/DirectoryScreen.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
 import { Button, View } from 'react-native';
+import React from 'react';
 
 const DirectoryScreen = () => {
   const navigation = useNavigation();
--- src/screens/NoteScreen.tsx
+++ src/screens/NoteScreen.tsx
@@ -1,18 +1,34 @@
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import styled from 'styled-components/native';
 import {
   Text,
   Platform,
-  KeyboardAvoidingView,
+  KeyboardAvoidingView as BaseKeyboardAvoidingView,
   SafeAreaView,
   ScrollView,
   ColorValue,
+  StyleSheet,
+  View,
 } from 'react-native';
 import {
   actions,
   RichEditor,
   RichToolbar,
 } from 'react-native-pell-rich-editor';
-import { useNavigation } from '@react-navigation/native';
+import { useKeyboard, useViewDimensions } from '../hooks';
+
+const EditorScrollView = styled(ScrollView)`
+  background-color: yellow;
+`;
+
+const EditorContainer = styled(View)`
+  background-color: blue;
+  height: 100px;
+`;
+
+const KeyboardAvoidingView = styled(BaseKeyboardAvoidingView)`
+  background-color: lightgreen;
+`;
 
 const handleHead = ({ tintColor }: { tintColor: ColorValue }) => (
   <Text style={{ color: tintColor }}>H1</Text>
@@ -20,35 +36,63 @@ const handleHead = ({ tintColor }: { tintColor: ColorValue }) => (
 
 const NoteScreen = () => {
   const richText = useRef<RichEditor>(null);
+  const { keyboardShown } = useKeyboard();
+  const [toolbarRef, dimensions] = useViewDimensions();
+
+  useEffect(() => {}, []);
 
   return (
-    <SafeAreaView>
-      <ScrollView>
+    <>
+      <EditorScrollView>
         <KeyboardAvoidingView
-          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-          style={{ flex: 1 }}>
+          behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
           <Text>Description:</Text>
-          <RichEditor
-            ref={richText}
-            onChange={descriptionText => {
-              console.log('descriptionText:', descriptionText);
-            }}
-          />
+          <EditorContainer>
+            <RichEditor
+              ref={richText}
+              onChange={descriptionText => {
+                console.log('descriptionText:', descriptionText);
+              }}
+              useContainer={false}
+            />
+          </EditorContainer>
         </KeyboardAvoidingView>
-      </ScrollView>
-
-      <RichToolbar
-        editor={richText}
-        actions={[
-          actions.setBold,
-          actions.setItalic,
-          actions.setUnderline,
-          actions.heading1,
-        ]}
-        iconMap={{ [actions.heading1]: handleHead }}
-      />
-    </SafeAreaView>
+      </EditorScrollView>
+      {keyboardShown && (
+        <View>
+          <RichToolbar
+            ref={richToolbarRef}
+            editor={richText}
+            actions={[
+              actions.undo,
+              actions.redo,
+              actions.setBold,
+              actions.setItalic,
+              actions.setUnderline,
+              actions.indent,
+              actions.outdent,
+              actions.alignLeft,
+              actions.alignCenter,
+              actions.alignRight,
+              actions.blockquote,
+              actions.checkboxList,
+              actions.fontSize,
+              actions.foreColor,
+              actions.heading1,
+            ]}
+            iconMap={{ [actions.heading1]: handleHead }}
+          />
+        </View>
+      )}
+    </>
   );
 };
 
 export default NoteScreen;
+
+const styles = StyleSheet.create({
+  editor: {
+    height: 300,
+    flex: 1,
+  },
+});
