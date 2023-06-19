bb14efc00dc143575f64dcc725aa0eab616a1292
John Hillert <john.hillert@gmail.com>
2023-06-18 20:56:53 -0500

    eslint-prettier dependency

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'prettier'],
   rules: {
     ['no-unused-vars']: 0,
     ['@typescript-eslint/no-unused-vars']: 0,
