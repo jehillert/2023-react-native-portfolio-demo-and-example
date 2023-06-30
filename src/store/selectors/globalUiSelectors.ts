25890c2556a7359283dfcd2bd0f360da22c0064d
John Hillert <john.hillert@gmail.com>
2023-06-30 17:50:57 -0500

    globalUiSelectors

--- src/store/selectors/globalUiSelectors.ts
+++ src/store/selectors/globalUiSelectors.ts
@@ -1,19 +1,9 @@
 import { RootState } from '../store';
 
-const selectActiveNoteId = (state: RootState) => state.globalUi.activeNoteId;
-
-const selectUntitledNoteCount = (state: RootState) =>
-  state.globalUi.untitledNoteCount;
-
 const selectLeftDrawerOpen = (state: RootState) =>
   state.globalUi.leftDrawerOpen;
 
 const selectRightDrawerOpen = (state: RootState) =>
   state.globalUi.rightDrawerOpen;
 
-export {
-  selectActiveNoteId,
-  selectLeftDrawerOpen,
-  selectRightDrawerOpen,
-  selectUntitledNoteCount,
-};
+export { selectLeftDrawerOpen, selectRightDrawerOpen };
--- src/store/slices/globalUiSlice.ts
+++ src/store/slices/globalUiSlice.ts
@@ -1,15 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface IglobalUi {
-  activeNoteId: string | null;
-  untitledNoteCount: number;
   leftDrawerOpen: boolean;
   rightDrawerOpen: boolean;
 }
 
 const initialState: IglobalUi = {
-  activeNoteId: null,
-  untitledNoteCount: 0,
   leftDrawerOpen: false,
   rightDrawerOpen: false,
 };
@@ -18,12 +14,6 @@ const globalUiSlice = createSlice({
   name: 'globalUi',
   initialState,
   reducers: {
-    activeNoteIdSet(state, { payload }: PayloadAction<string>) {
-      state.activeNoteId = payload;
-    },
-    untitledNoteCountSet(state, { payload }: PayloadAction<number>) {
-      state.untitledNoteCount = payload;
-    },
     leftDrawerOpened(state, { payload }: PayloadAction<boolean>) {
       state.leftDrawerOpen = payload;
     },
@@ -33,11 +23,6 @@ const globalUiSlice = createSlice({
   },
 });
 
-export const {
-  activeNoteIdSet,
-  leftDrawerOpened,
-  rightDrawerOpened,
-  untitledNoteCountSet,
-} = globalUiSlice.actions;
+export const { leftDrawerOpened, rightDrawerOpened } = globalUiSlice.actions;
 
 export default globalUiSlice.reducer;
