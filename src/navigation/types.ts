68eab273b522a5f5a0fac8413d6a80cb2c7f5600
John Hillert <john.hillert@gmail.com>
2023-06-30 17:50:23 -0500

    nav types without drawers

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -1,6 +1,4 @@
-import type { CompositeScreenProps } from '@react-navigation/native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { DrawerScreenProps } from '@react-navigation/drawer';
 
 enum Screens {
   DIRECTORY = 'Directory',
@@ -11,46 +9,25 @@ enum Screens {
 
 type CommonNavParams = { id?: string } | undefined;
 
-type DrawerParamList = {
-  [Screens.DIRECTORY]: CommonNavParams;
-  [Screens.HOME]: undefined;
-  [Screens.NOTE]: CommonNavParams;
-  [Screens.MARKUP]: CommonNavParams;
-};
-
 type StackParamList = {
   [Screens.DIRECTORY]: CommonNavParams;
   [Screens.NOTE]: CommonNavParams;
   [Screens.MARKUP]: CommonNavParams;
 };
 
-type DrawerNavScreenProps<T extends keyof DrawerParamList> = DrawerScreenProps<
-  DrawerParamList,
-  T
+type DirectoryScreenProps = NativeStackScreenProps<
+  StackParamList,
+  Screens.DIRECTORY
 >;
 
-type DirectoryScreenProps = CompositeScreenProps<
-  NativeStackScreenProps<StackParamList, Screens.DIRECTORY>,
-  DrawerNavScreenProps<Screens.DIRECTORY>
->;
-
-type NoteScreenProps = CompositeScreenProps<
-  NativeStackScreenProps<StackParamList, Screens.NOTE>,
-  DrawerNavScreenProps<Screens.NOTE>
->;
+type NoteScreenProps = NativeStackScreenProps<StackParamList, Screens.NOTE>;
 
 declare global {
   namespace ReactNavigation {
-    interface RootParamList extends DrawerParamList {}
+    interface RootParamList extends StackParamList {}
   }
 }
 
-export type {
-  DrawerParamList,
-  StackParamList,
-  DrawerNavScreenProps,
-  DirectoryScreenProps,
-  NoteScreenProps,
-};
+export type { StackParamList, DirectoryScreenProps, NoteScreenProps };
 
 export { Screens };
